57248be3af853cde5e2fb6c5fde60f0806d49d4e
roopaksai543 <roopaksai543@gmail.com>
2025-08-06 19:31:31 -0700

    Create V6.ino

---
+++ codeVersions/V6.ino
@@ -0,0 +1,510 @@
+#include <Wire.h>
+#include <RTClib.h>
+#include <LiquidCrystal_I2C.h>
+
+// Time-tracking Set Up
+const char* daysOfTheWeek[] = {
+  "Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"
+};
+
+// RTC Setup
+RTC_DS3231 rtc;
+DateTime now;
+
+// killSwitch Setup
+int killSwitchButtonPin = 7;
+
+// Class for first option of goal setting
+class firstOptionLCD {
+
+  public:
+  String goalName = "";
+  int goalDay1 = 0;
+  int goalDay2 = 0;
+  String originalSecondRowLine = "";
+  String lastSecondRowLine = "";
+  int totalDaysPassed = 0;
+  int changingGoalDay = 0;
+  int savedHour;
+  int savedMinute;
+  // Button Set Up
+  int buttonPin = 0;
+  int buttonState = 0;
+  bool hasBeenPressed = false;
+  bool buttonHeld = false;
+  unsigned long pressStart = 0;
+  //I/O 
+  int lcdAddress;
+  //LiquidCrystal lcd;
+  LiquidCrystal_I2C lcd;
+
+  // Constructor
+  firstOptionLCD(String goalName_, int goalDay1_, int goalDay2_, int lcdAddress_, int buttonPin_) : lcd(lcdAddress_, 16, 2) {
+    goalName = goalName_;
+    goalDay1 = goalDay1_;
+    goalDay2 = goalDay2_;
+    lcdAddress = lcdAddress_;
+    buttonPin = buttonPin_;
+  }
+
+  void lcdSetup() {
+
+    // Makes sure the larger one is goalDay2 and smaller is goalDay1
+    arrangeGoalDays();
+
+    // More LCD1 Set Up
+    lcd.init();
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print(goalName);
+
+    // Check if rtc is working
+    if (!rtc.begin()) {
+      lcd.setCursor(0, 0);
+      lcd.print("RTC failed");
+      // while (1); // Halt
+    }
+
+    setGoalDay(); // Sets the initial changingGoalDay
+    checkAndChangeOutput(); // Sets the initial output for second row
+
+    pinMode(buttonPin, INPUT_PULLUP); // Button Setup
+    
+  }
+
+  // Makes sure the larger one is goalDay2 and smaller is goalDay1
+  void arrangeGoalDays() {
+
+    if (goalDay2 < goalDay1) {
+        int temp = goalDay1;
+        goalDay1 = goalDay2;
+        goalDay2 = temp;
+    } 
+  }
+
+  // Sets the initial changingGoalDay
+  void setGoalDay() {
+
+    totalDaysPassed += now.dayOfTheWeek();
+    if (now.dayOfTheWeek() <= goalDay1) {
+        changingGoalDay += goalDay1;
+    }
+    else if (now.dayOfTheWeek() <= goalDay2) {
+        changingGoalDay += goalDay2;
+    }
+    else {
+        changingGoalDay += 7 + goalDay1;
+    }
+
+    savedHour = now.hour();
+    savedMinute = now.minute();
+  }
+
+  // Increments the totalDaysPassed properly
+  void incrementDaysPassed() {
+    
+    // Update totalDaysPassed
+    if ((totalDaysPassed%7) != now.dayOfTheWeek()) {
+      totalDaysPassed++;
+      if (((totalDaysPassed%7 > goalDay1) && hasBeenPressed) || (((totalDaysPassed%7) > goalDay2) && hasBeenPressed)) {
+          hasBeenPressed = false;
+      }
+      checkAndChangeOutput();
+    }
+
+    // Update savedHour and minute
+    if (savedHour != now.hour()) {
+      savedHour = now.hour();
+    }
+    if (savedMinute != now.minute()) {
+      savedMinute = now.minute();
+    }
+
+    if (totalDaysPassed == changingGoalDay) {
+      if (now.hour() == 23) {
+      checkAndChangeOutput();
+    }
+    }
+  }
+
+  // Prints second row of information
+  void printSecondRow() {
+
+      lcd.setCursor(0, 1);
+      lcd.print("                ");
+      lcd.setCursor(0, 1);
+      lcd.print(originalSecondRowLine.substring(0, 16));
+      lastSecondRowLine = originalSecondRowLine;
+
+  }
+
+  // Checks to see if any information has changed and changes the output
+  void checkAndChangeOutput() {
+
+    if (totalDaysPassed <= changingGoalDay && hasBeenPressed == false) {
+      if (totalDaysPassed == changingGoalDay) {
+        originalSecondRowLine = "";
+        originalSecondRowLine += 23 - now.hour();
+        originalSecondRowLine += "h Left - ";
+        originalSecondRowLine += daysOfTheWeek[(changingGoalDay%7)];
+        if (now.hour() == 23) {
+          originalSecondRowLine = "";
+          originalSecondRowLine += 59 - now.minute();
+          originalSecondRowLine += "m Left - ";
+          originalSecondRowLine += daysOfTheWeek[(changingGoalDay%7)];
+        }
+      }
+      else {
+        originalSecondRowLine = "";
+        originalSecondRowLine += changingGoalDay - totalDaysPassed;
+        originalSecondRowLine += "d Left - ";
+        originalSecondRowLine += daysOfTheWeek[(changingGoalDay%7)];
+      }
+    }
+    else if (totalDaysPassed <= changingGoalDay && hasBeenPressed == true) {
+      originalSecondRowLine = "";
+      originalSecondRowLine = "Task complete.  ";
+    }
+    else if (totalDaysPassed > changingGoalDay) {
+      originalSecondRowLine = "";
+      originalSecondRowLine += totalDaysPassed - changingGoalDay;
+      originalSecondRowLine += "d Late - ";
+      originalSecondRowLine += daysOfTheWeek[(changingGoalDay%7)];
+    }
+
+    printSecondRow();
+
+  }
+
+  // Increments the changingGoalDay depending on parameters
+  void incrementGoalDay() {
+
+    if (goalDay1 == goalDay2) {
+        changingGoalDay += 7;
+    }  
+    else if ((changingGoalDay%7) == goalDay1) {
+        changingGoalDay += goalDay2 - goalDay1;
+    }
+    else if ((changingGoalDay%7) == goalDay2) {
+        changingGoalDay +=  7 - goalDay2 + goalDay1;
+    }
+      
+  }
+  
+  // Decrements the changingGoalDay depending on parameters
+  void decrementGoalDay() {
+
+    if (goalDay1 == goalDay2) {
+      changingGoalDay -= 7;
+    }
+    else if ((changingGoalDay%7) == goalDay1) {
+      changingGoalDay -= goalDay1;
+      changingGoalDay -= (7-goalDay2);
+    }
+    else if ((changingGoalDay%7) == goalDay2) {
+      changingGoalDay -=  (goalDay2 - goalDay1);
+    }
+      
+    hasBeenPressed = false;
+  }
+
+  // Checks if button has been pressed and for how long
+  void checkButton() {
+
+    int state = digitalRead(buttonPin);
+
+    // Button just pressed
+    if (state == LOW && !buttonHeld) {
+      pressStart = millis();
+      buttonHeld = true;
+
+      lcd.setCursor(0, 1);
+      lcd.print("                ");
+      
+    }
+
+    // Button just released
+    if (state == HIGH && buttonHeld) {
+      unsigned long pressDuration = millis() - pressStart;
+      
+      if (pressDuration < 250) {
+        // Short press logic
+        if (changingGoalDay < totalDaysPassed) {
+          while (changingGoalDay < totalDaysPassed) {
+            incrementGoalDay();
+          }
+          hasBeenPressed = false;
+        } 
+        else if (hasBeenPressed == false) {
+          incrementGoalDay();
+          hasBeenPressed = true;
+        }
+        lcd.setCursor(0, 1);
+        lcd.print("Task complete.  ");
+        delay(250);
+        lcd.setCursor(0, 1);
+        lcd.print("                ");
+        delay(250);
+        checkAndChangeOutput();
+
+      } else {
+        // Long press logic
+        decrementGoalDay();
+        lcd.setCursor(0, 1);
+        lcd.print("Reset.          ");
+        delay(250);
+        checkAndChangeOutput();
+      }
+
+      // Reset state
+      buttonHeld = false;
+    }
+  }
+
+  // Turns off backlight
+  void offBacklight() {
+    lcd.noBacklight();
+  }
+
+  // Turns on backlight
+  void onBacklight() {
+    lcd.backlight();
+  }
+};
+
+// Class for second option of goal setting
+class secondOptionLCD {
+
+  public:
+  String goalName = "";
+  int resetDay = 0;
+  String originalSecondRowLine = "";
+  int timesPerWeek;
+  int savedWeekday = 0;
+  // Button Set Up
+  int buttonPin = 0;
+  int buttonState = 0;
+  bool hasBeenPressed = false;
+  bool buttonHeld = false;
+  unsigned long pressStart = 0;
+  int numOfTimesPressed = 0;
+  //I/O 
+  int lcdAddress;
+  //LiquidCrystal lcd;
+  LiquidCrystal_I2C lcd;
+
+  // Constructor
+  secondOptionLCD(String goalName_, int resetDay_, int timesPerWeek_, int lcdAddress_, int buttonPin_) : lcd(lcdAddress_, 16, 2) {
+    goalName = goalName_;
+    resetDay = resetDay_;
+    timesPerWeek = timesPerWeek_;
+    lcdAddress = lcdAddress_;
+    buttonPin = buttonPin_;
+  }
+
+  void lcdSetup() {
+
+    // More LCD1 Set Up
+    lcd.init();
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print(goalName);
+
+    if (!rtc.begin()) {
+      lcd.setCursor(0, 0);
+      lcd.print("RTC failed");
+      //while (1); // Halt
+    }
+
+
+    checkAndChangeOutput(); // Sets the initial output for second row
+
+    pinMode(buttonPin, INPUT_PULLUP); // Button Setup
+    
+  }
+
+  // Increments the totalDaysPassed properly
+  void incrementDaysPassed() {
+    
+    // Update totalDaysPassed
+    if (savedWeekday != now.dayOfTheWeek()) {
+      savedWeekday = now.dayOfTheWeek();
+      if (savedWeekday == resetDay) {
+        numOfTimesPressed = 0;
+      }
+      checkAndChangeOutput();
+    }
+  }
+
+  // Prints second row of information
+  void printSecondRow() {
+
+    lcd.setCursor(0, 1);
+    lcd.print(originalSecondRowLine.substring(0, 16));
+
+  }
+
+  // Checks to see if any information has changed and changes the output
+  void checkAndChangeOutput() {
+
+    if (timesPerWeek > numOfTimesPressed && now.dayOfTheWeek() <= resetDay) {
+      originalSecondRowLine = "";
+      originalSecondRowLine += numOfTimesPressed;
+      originalSecondRowLine += "/";
+      originalSecondRowLine += timesPerWeek; 
+      originalSecondRowLine += "  ";
+      originalSecondRowLine += resetDay - now.dayOfTheWeek();
+      originalSecondRowLine += "d - ";
+      originalSecondRowLine += daysOfTheWeek[resetDay];
+    }
+    else if (timesPerWeek > numOfTimesPressed && now.dayOfTheWeek() > resetDay) {
+      originalSecondRowLine = "";
+      originalSecondRowLine += numOfTimesPressed;
+      originalSecondRowLine += "/";
+      originalSecondRowLine += timesPerWeek; 
+      originalSecondRowLine += "  ";
+      originalSecondRowLine += 7 - now.dayOfTheWeek() + resetDay;
+      originalSecondRowLine += "d - ";
+      originalSecondRowLine += daysOfTheWeek[resetDay];
+    }
+    else if (timesPerWeek <= numOfTimesPressed) {
+      originalSecondRowLine = "";
+      originalSecondRowLine += "Task complete.";
+    }
+
+    printSecondRow();
+
+  }
+
+  // Checks if button has been pressed and for how long
+  void checkButton() {
+
+    int state = digitalRead(buttonPin);
+
+    // Button just pressed
+    if (state == LOW && !buttonHeld) {
+      pressStart = millis();
+      buttonHeld = true;
+
+      lcd.setCursor(0, 1);
+      lcd.print("                ");
+      
+    }
+
+    // Button just released
+    if (state == HIGH && buttonHeld) {
+      unsigned long pressDuration = millis() - pressStart;
+      
+      if (pressDuration < 250) {
+        // Short press logic
+        numOfTimesPressed++;
+        if (numOfTimesPressed > timesPerWeek) {
+          numOfTimesPressed = timesPerWeek;
+        }
+        String temp = "";
+        temp += "Complete. ";
+        temp += numOfTimesPressed;
+        temp += "/";
+        temp += timesPerWeek;
+        lcd.setCursor(0, 1);
+        lcd.print("Task complete.  ");
+        delay(250);
+        lcd.setCursor(0, 1);
+        lcd.print("                ");
+        delay(250);
+        checkAndChangeOutput();
+
+      } else {
+        // Long press logic
+        numOfTimesPressed--;
+        if (numOfTimesPressed < 0) {
+          numOfTimesPressed = 0;
+        }
+        lcd.setCursor(0, 1);
+        lcd.print("Reset.          ");
+        delay(250);
+        checkAndChangeOutput();
+      }
+
+      // Reset state
+      buttonHeld = false;
+    }
+  }
+
+  // Turns off backlight
+  void offBacklight() {
+    lcd.noBacklight();
+  }
+
+  // Turns on backlight
+  void onBacklight() {
+    lcd.backlight();
+  }
+
+
+};
+
+// Initializing both LCD's
+{{COMPLETED_DECLARATION_H1}}
+{{COMPLETED_DECLARATION_H2}}
+{{COMPLETED_DECLARATION_H3}}
+{{COMPLETED_DECLARATION_H4}}
+{{COMPLETED_DECLARATION_H5}}
+
+void checkKillSwitch() {
+
+  if (digitalRead(killSwitchButtonPin)) {
+    lcd1.onBacklight();
+    lcd2.onBacklight();
+    lcd3.onBacklight();
+    lcd4.onBacklight();
+    lcd5.onBacklight();
+  } else {
+    lcd1.offBacklight();
+    lcd2.offBacklight();
+    lcd3.offBacklight();
+    lcd4.offBacklight();
+    lcd5.offBacklight();
+  }
+
+}
+
+void setup() {
+
+  // Used for debugging
+  Serial.begin(9600);
+
+  // lcd1 and lcd2 Setup
+  lcd1.lcdSetup();
+  lcd2.lcdSetup();
+  lcd3.lcdSetup();
+  lcd4.lcdSetup();
+  lcd5.lcdSetup();
+
+  // killSwitch Setup
+  pinMode(7, INPUT_PULLUP);
+
+  // Rest time - Only call once if time needs to be reset
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  
+}
+
+void loop() {  
+
+  now = rtc.now();
+
+  lcd1.checkButton(); // Checks if button has been pressed
+  lcd1.incrementDaysPassed(); // Increments totalDaysPassed if weekday() changes
+  lcd2.checkButton(); // Checks if button has been pressed
+  lcd2.incrementDaysPassed(); // Increments totalDaysPassed if weekday() changes
+  lcd3.checkButton(); // Checks if button has been pressed
+  lcd3.incrementDaysPassed(); // Increments totalDaysPassed if weekday() changes
+  lcd4.checkButton(); // Checks if button has been pressed
+  lcd4.incrementDaysPassed(); // Increments totalDaysPassed if weekday() changes
+  lcd5.checkButton(); // Checks if button has been pressed
+  lcd5.incrementDaysPassed(); // Increments totalDaysPassed if weekday() changes
+
+  // killSwitch code
+  checkKillSwitch();
+  
+}
